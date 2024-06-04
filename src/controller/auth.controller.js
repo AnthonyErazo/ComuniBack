74fb14faeff4e6695821503276e9d85d07446fed
Anthony Erazo <anthonyerazo76@gmail.com>
2024-06-03 22:47:11 -0500

    modificacion logout

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -12,7 +12,7 @@ class AuthController {
         try {
             const requieredfield = ['email', 'password'];
             const userData = validateFields(req.body, requieredfield);
-            const userFound = await userService.getUser({ email: userData.email },true);
+            const userFound = await userService.getUser({ email: userData.email }, true);
             if (userFound.payload._id) {
                 throw new Error("Ya existe un usuario con ese email")
             }
@@ -21,7 +21,7 @@ class AuthController {
             const newUser = {
                 email: userData.email,
                 password: createHash(userData.password),
-                group:group.payload._id
+                group: group.payload._id
             };
 
             await userService.createUser(newUser);
@@ -51,20 +51,20 @@ class AuthController {
                     sameSite: 'None',
                     credentials: true
                 });
-                return res.status(200).json({status:'success', message: 'Login successful' });
+                return res.status(200).json({ status: 'success', message: 'Login successful' });
             }
 
             const { payload: userFound } = await userService.getUser({ email: userData.email }, false);
-            if(!userFound._id){
+            if (!userFound._id) {
                 throw new Error('Usuario no existe')
             }
             if (!userFound || !isValidPassword(userData.password, { password: userFound.password })) {
                 throw new Error(`Contraseña equivocado`)
             }
-            const token = createToken({ 
+            const token = createToken({
                 id: userFound._id,
                 role: 'user',
-                group:userFound.group._id
+                group: userFound.group._id
             })
             res.cookie(configObject.COOKIE_AUTH, token, {
                 maxAge: 60 * 60 * 1000 * 24,
@@ -73,7 +73,7 @@ class AuthController {
                 sameSite: 'None',
                 credentials: true
             });
-            return res.status(200).json({ status:'success',message: 'Login successful' });
+            return res.status(200).json({ status: 'success', message: 'Login successful' });
         } catch (error) {
             console.error(error);
             return res.status(500).json({
@@ -83,18 +83,23 @@ class AuthController {
         }
     }
     logout = async (req, res) => {
-        req.user=null
-        res.clearCookie(configObject.COOKIE_AUTH);
-        return res.status(200).send({ status:'success',message: 'Logout successful' })
+        req.user = null
+        res.clearCookie(configObject.COOKIE_AUTH, {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'None',
+            credentials: true
+        });
+        return res.status(200).send({ status: 'success', message: 'Logout successful' })
     }
 
-    
+
     forgotPassword = async (req, res) => {
         try {
             const { email } = req.body;
             const user = await userService.getUser({ email }, true);
 
-            const token = jwt.sign({userId:user.payload._id}, configObject.JWT_PRIVATE_KEY, { expiresIn: '1h' });
+            const token = jwt.sign({ userId: user.payload._id }, configObject.JWT_PRIVATE_KEY, { expiresIn: '1h' });
 
             const resetLink = `http://localhost:8080/reset-password?token=${token}`;
 
@@ -117,23 +122,23 @@ class AuthController {
     }
     resetPassword = async (req, res) => {
         try {
-            const {token} = req.params;
-            const  {password}  = req.body;
+            const { token } = req.params;
+            const { password } = req.body;
             const decoded = jwt.verify(token, configObject.JWT_PRIVATE_KEY);
             const userId = decoded.userId;
 
-            const {payload} = await userService.getUser({ _id: userId }, false);
+            const { payload } = await userService.getUser({ _id: userId }, false);
             if (isValidPassword(password, { password: payload.password })) {
                 throw new Error(`La contraseña no pueden ser iguales`)
             }
-            await userService.updateUser(userId,{password:createHash(password)})
+            await userService.updateUser(userId, { password: createHash(password) })
 
             return res.status(200).json({ message: 'Contraseña restablecida' });
         } catch (error) {
             console.error(error);
-            return res.status(500).json({ 
-                status:'error',
-                message: error.message 
+            return res.status(500).json({
+                status: 'error',
+                message: error.message
             });
         }
     };
